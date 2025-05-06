c67e480d7ea041a486e9623b418cc7b7afcd1588
demercurizer <gotovskii.gd@phystech.edu>
2025-05-06 18:30:14 +0300

    add CmakeLists

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/gennady/Programming/test_task_yadro/tests
+# Build directory: /home/gennady/Programming/test_task_yadro/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(ClubSystemTests "/home/gennady/Programming/test_task_yadro/build/tests/runTests")
+set_tests_properties(ClubSystemTests PROPERTIES  WORKING_DIRECTORY "/home/gennady/Programming/test_task_yadro/build" _BACKTRACE_TRIPLES "/home/gennady/Programming/test_task_yadro/tests/CMakeLists.txt;22;add_test;/home/gennady/Programming/test_task_yadro/tests/CMakeLists.txt;0;")
